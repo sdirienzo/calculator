9c78185fa23c717f25e138c4f58be414f277112f
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-07-23 20:00:33 -0400

    Introduce operate function

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,26 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    let result = 0;
+
+    switch (operator) {
+        case "add":
+            result = add(a, b);
+            break;
+        case "subtract":
+            result = subtract(a, b);
+            break;
+        case "multiply":
+            result = multiply(a, b);
+            break;
+        case "divide":
+            result = divide(a, b);
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
